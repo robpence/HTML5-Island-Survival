26cfe1b15ec6ded446ed2a271149a6438c3139e2
Robert <rpence12395@gmail.com>
2017-09-23 18:31:29 -0700

    Can now drag the map with the mouse, TODO choose between this or making the map move when the mouse is at the boundraries

--- public/states/Game.js
+++ public/states/Game.js
@@ -234,6 +234,7 @@ Game.prototype = {
 		var plantCuttingBarConfig = {x: -100, y: -100, width: 32, height: 8}
 		plantCuttingBar = new HealthBar(game, plantCuttingBarConfig);
 		plantCuttingBar.setBarColor('#6DC066');
+
 	},
 
 	update() {
@@ -304,7 +305,18 @@ Game.prototype = {
 		//---------- KEYBOARD INPUTS -------------
 		//----------------------------------------
 		listenForKeyboardInputs();
-			
+
+		if (this.game.input.activePointer.isDown) {	
+			if (this.game.origDragPoint) {
+				// move the camera by the amount the mouse has moved since last update		
+				this.game.camera.x += this.game.origDragPoint.x - this.game.input.activePointer.position.x;		
+				this.game.camera.y += this.game.origDragPoint.y - this.game.input.activePointer.position.y;	
+			}	
+			// set new drag origin to current position	
+			this.game.origDragPoint = this.game.input.activePointer.position.clone();
+		}else {	
+			this.game.origDragPoint = null;
+		}
 
 		char2Wander();
 
